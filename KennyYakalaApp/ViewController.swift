612b59d99f6d1d5ee960cca82488bd0d59ee9ca9
atilla.citirik <atillactrk@gmail.com>
2023-12-01 18:54:54 +0300

    Initial Commit

---
+++ KennyYakalaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KennyYakalaApp
+//
+//  Created by Atilla Çıtırık on 1.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
